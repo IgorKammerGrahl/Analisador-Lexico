8dc610c38b82520eecba3590d700e723df9aa99a
Igor <igorkramigra@gmail.com>
2024-10-09 16:05:50 -0300

    Atualizacao

--- analiseLexica.php
+++ analiseLexica.php
@@ -3,119 +3,287 @@ header("Content-type: text/html; charset=utf-8");
 
 $codigo = isset($_POST["codigo"]) ? $_POST["codigo"] : "";
 
-function verificaCaracter($caracter, $apenasLetras){
-    for($i=65; $i<=90; $i++){
-        if(strtoupper($caracter) == chr($i)){
-            return true;
-        }
+class Automato {
+    private $estadoInicial;
+    private $estadosFinais;
+    private $transicoes;
+
+    public function __construct($estadoInicial, $estadosFinais, $transicoes) {
+        $this->estadoInicial = $estadoInicial;
+        $this->estadosFinais = $estadosFinais;
+        $this->transicoes = $transicoes;
     }
 
-    if(!$apenasLetras){
-        for($i=48; $i<=57; $i++){
-            if($caracter == chr($i)){
-                return true;
-            }
-        }
+    public function executa($palavra) {
+        $estadoAtual = $this->estadoInicial;
 
-        if($caracter == "_"){
-            return true;
-        }
-    }
+        for ($i = 0; $i < strlen($palavra); $i++) {
+            $caracter = $palavra[$i];
 
-    return false;
-}
-
-function reservadas(){
-    return array("auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else",
-                 "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", 
-                 "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", 
-                 "unsigned", "void", "volatile", "while", "asm", "cdecl", "far", "fortran", "huge", 
-                 "interrupt", "near", "pascal", "typeof");
-}
-
-function operadores(){
-    return array("+" => "matemático de soma",
-                 "-" => "matemático de subtração",
-                 "*" => "matemático de multiplicação",
-                 "/" => "matemático de divisão",
-                 "=" => "de atribuição",
-                 "==" => "lógico de igualdade",
-                 "!=" => "lógico de diferença",
-                 "%" => "matemático de percentual",
-                 ">" => "lógico, indicando maior",
-                 "<" => "lógico, indicando menor",
-                 "<=" => "lógico, indicando menor ou igual",
-                 ">=" => "lógico, indicando maior igual",
-                 "&&" => "relacional, indicando \"e\"",
-                 "||" => "relacional, indicando \"ou\"");
-}
-
-function ehNumero($caracter){
-    return $caracter >= '0' && $caracter <= '9';
-}
-
-function ehLetra($caracter){
-    return ($caracter >= 'a' && $caracter <= 'z') || ($caracter >= 'A' && $caracter <= 'Z');
-}
-
-function analiseLexica($codigo){
-    $reservadas = reservadas();
-    $operadores = operadores();
-    $resultados = [];
-    $token = "";
-    $comprimento = strlen($codigo);
-    
-    for($i = 0; $i < $comprimento; $i++){
-        $caracter = $codigo[$i];
-        
-        if($caracter == ' ' || $caracter == "\n" || $caracter == "\t"){
-            continue;
-        }
-
-        if(ehLetra($caracter) || $caracter == '_'){
-            $token = $caracter;
-            while($i + 1 < $comprimento && (ehLetra($codigo[$i + 1]) || ehNumero($codigo[$i + 1]) || $codigo[$i + 1] == '_')){
-                $token .= $codigo[++$i];
-            }
-
-            if(in_array($token, $reservadas)){
-                $resultados[] = "<b>$token</b> é uma palavra reservada.";
+            if (isset($this->transicoes[$estadoAtual][$caracter])) {
+                $estadoAtual = $this->transicoes[$estadoAtual][$caracter];
             } else {
-                $resultados[] = "<b>$token</b> pode ser o nome de uma variável ou procedimento.";
+                return false;
             }
-            continue;
         }
 
-        if(ehNumero($caracter)){
-            $token = $caracter;
-            while($i + 1 < $comprimento && ehNumero($codigo[$i + 1])){
-                $token .= $codigo[++$i];
-            }
-
-            $resultados[] = "<b>$token</b> é um número.";
-            continue;
-        }
-
-        if(array_key_exists($caracter, $operadores)){
-            $token = $caracter;
-            if($i + 1 < $comprimento && array_key_exists($caracter . $codigo[$i + 1], $operadores)){
-                $token .= $codigo[++$i];
-            }
-
-            $resultados[] = "<b>$token</b> é um operador " . $operadores[$token] . ".";
-            continue;
-        }
-
-        if(in_array($caracter, ['(', ')', '{', '}', '[', ']'])){
-            $resultados[] = "<b>$caracter</b> é um delimitador.";
-            continue;
-        }
-
-        $resultados[] = "<b>$caracter</b> causará erro de sintaxe.";
+        return in_array($estadoAtual, $this->estadosFinais);
     }
-
-    return implode("<br>", $resultados);
 }
 
-echo analiseLexica($codigo);
+// Função para criar autômatos
+function criaAutomatos() {
+    // Autômato para palavras reservadas
+    $palavrasReservadas = new Automato(
+        'q0',
+        ['q2', 'q3', 'q4'], // Estados finais que indicam palavras reservadas
+        [
+            'q0' => ['i' => 'q1', 'e' => 'q10', 'w' => 'q20'],
+            'q1' => ['f' => 'q2'],  // Reconhece "if"
+            'q10' => ['l' => 'q11'],
+            'q11' => ['s' => 'q12'],
+            'q12' => ['e' => 'q3'],  // Reconhece "else"
+            'q20' => ['h' => 'q21'],
+            'q21' => ['i' => 'q22'],
+            'q22' => ['l' => 'q23'],
+            'q23' => ['e' => 'q4'],  // Reconhece "while"
+        ]
+    );
+
+    // Autômato para identificadores
+    $identificador = new Automato(
+        'q0',
+        ['q1'],
+        [
+            'q0' => array_merge(array_fill_keys(range('a', 'z'), 'q1'), array_fill_keys(range('A', 'Z'), 'q1')),
+            'q1' => array_merge(array_fill_keys(range('a', 'z'), 'q1'), array_fill_keys(range('A', 'Z'), 'q1'), array_fill_keys(range('0', '9'), 'q1')),
+        ]
+    );
+
+    // Autômato para constantes numéricas
+    $constante = new Automato(
+        'q0',
+        ['q1'],
+        [
+            'q0' => array_fill_keys(range('0', '9'), 'q1'),
+            'q1' => array_fill_keys(range('0', '9'), 'q1'),
+        ]
+    );
+
+    // Autômato para operadores e delimitadores
+    $operadores = new Automato(
+        'q0',
+        ['q1', 'q3', 'q120'],
+        [
+            'q0' => [
+                '==' => 'q120', '=' => 'q120', 
+                '+' => 'q1', '-' => 'q1', '*' => 'q1', '/' => 'q1', '%' => 'q1',
+                '(' => 'q1', ')' => 'q1', '[' => 'q1', ']' => 'q1', '{' => 'q1', '}' => 'q1', 
+                '.' => 'q1', ',' => 'q1', ';' => 'q1', '!' => 'q1'
+            ],
+            'q120' => ['=' => 'q1']
+        ]
+    );
+
+    return [
+        'PALAVRARESERVADA' => $palavrasReservadas,
+        'IDENTIFICADOR' => $identificador,
+        'CONSTANTE' => $constante,
+        'OPERADOR' => $operadores
+    ];
+}
+
+// Função que realiza a análise léxica
+function lexer($sourceCode) {
+    $tokens = [];
+    $automatos = criaAutomatos();
+    $length = strlen($sourceCode);
+    $i = 0;
+    
+    // Iniciar contadores de linha e coluna
+    $linha = 1;
+    $coluna = 1;
+
+    while ($i < $length) {
+        $char = $sourceCode[$i];
+
+        if (ctype_space($char)) {
+            if ($char === "\n") {
+                $linha++;
+                $coluna = 1;
+            } else {
+                $coluna++;
+            }
+            $i++;
+            continue;
+        }
+
+        $word = '';
+        $startColuna = $coluna;
+
+        if ($char == '"') {
+            $i++;
+            $coluna++;
+            while ($i < $length && $sourceCode[$i] != '"') {
+                $word .= $sourceCode[$i];
+                $i++;
+                $coluna++;
+            }
+
+            if ($i < $length && $sourceCode[$i] == '"') {
+                $tokens[] = ['STRING', '"' . $word . '"'];
+                $i++;
+                $coluna++;
+            } else {
+                throw new Exception("Erro léxico: string não terminada na linha $linha, coluna $coluna.");
+            }
+            continue;
+        }
+
+        if (ctype_alpha($char)) {
+            while ($i < $length && ctype_alnum($sourceCode[$i])) {
+                $word .= $sourceCode[$i];
+                $i++;
+                $coluna++;
+            }
+        } elseif (ctype_digit($char)) {
+            while ($i < $length && ctype_digit($sourceCode[$i])) {
+                $word .= $sourceCode[$i];
+                $i++;
+                $coluna++;
+            }
+        } else {
+            if ($char == '=') {
+                while ($i < $length && $sourceCode[$i] == '=') {
+                    $word .= $sourceCode[$i];
+                    $i++;
+                    $coluna++;
+                }
+            } else {
+                $word .= $sourceCode[$i];
+                $i++;
+                $coluna++;
+            }
+        }
+
+        $found = false;
+
+        foreach ($automatos as $token => $automato) {
+            if ($automato->executa($word)) {
+                $tokens[] = [$token, $word];
+                $found = true;
+                break;
+            }
+        }
+
+        if (!$found) {
+            throw new Exception("Erro léxico: token desconhecido '$word' na linha $linha, coluna $startColuna.");
+        }
+    }
+
+    return $tokens;
+}
+
+// Executar a análise léxica e mostrar os resultados
+// Executar a análise léxica e mostrar os resultados
+try {
+    $tokens = lexer($codigo); // Certifique-se de que você está chamando a função correta
+    echo "<h2>Tokens Encontrados:</h2><pre>";
+    
+    foreach ($tokens as $token) {
+        // Descrição dos tokens
+        $descricao = '';
+        switch ($token[0]) {
+            case 'PALAVRARESERVADA':
+                switch ($token[1]) {
+                    case 'if':
+                        $descricao = 'Palavra Reservada: Condicional If';
+                        break;
+                    case 'else':
+                        $descricao = 'Palavra Reservada: Condicional Else';
+                        break;
+                    case 'while':
+                        $descricao = 'Palavra Reservada: Laço While';
+                        break;
+                    case 'for':
+                        $descricao = 'Palavra Reservada: Laço For';
+                        break;
+                    case 'function':
+                        $descricao = 'Palavra Reservada: Declaração de Função';
+                        break;
+                    // Adicione mais palavras reservadas conforme necessário
+                    default:
+                        $descricao = 'Palavra Reservada: ' . $token[1];
+                }
+                break;
+
+            case 'IDENTIFICADOR':
+                $descricao = 'Identificador: ' . $token[1];
+                break;
+
+            case 'CONSTANTE':
+                $descricao = 'Constante Numérica: ' . $token[1];
+                break;
+
+            case 'OPERADOR':
+                switch ($token[1]) {
+                    case '+':
+                        $descricao = 'Operador de Adição: ' . $token[1];
+                        break;
+                    case '-':
+                        $descricao = 'Operador de Subtração: ' . $token[1];
+                        break;
+                    case '*':
+                        $descricao = 'Operador de Multiplicação: ' . $token[1];
+                        break;
+                    case '/':
+                        $descricao = 'Operador de Divisão: ' . $token[1];
+                        break;
+                    case '=':
+                        $descricao = 'Operador de Atribuição: ' . $token[1];
+                        break;
+                    case '==':
+                        $descricao = 'Operador de Igualdade: ' . $token[1];
+                        break;
+                    case '(':
+                        $descricao = 'Abre Parênteses: ' . $token[1];
+                        break;
+                    case ')':
+                        $descricao = 'Fecha Parênteses: ' . $token[1];
+                        break;
+                    case '[':
+                        $descricao = 'Abre colchetes: ' . $token[1];
+                        break;
+                    case ']':
+                        $descricao = 'Fecha colchetes: ' . $token[1];
+                        break;
+                    case '{':
+                        $descricao = 'Abre chaves: ' . $token[1];
+                        break;
+                    case '}':
+                        $descricao = 'Fecha chaves: ' . $token[1];
+                        break;
+                    // Adicione mais operadores conforme necessário
+                    default:
+                        $descricao = 'Operador: ' . $token[1];
+                }
+                break;
+
+            case 'STRING':
+                $descricao = 'String: ' . $token[1];
+                break;
+
+            default:
+                $descricao = 'Token Desconhecido: ' . $token[1];
+        }
+        
+        echo "{$descricao}\n";
+    }
+    echo "</pre>";
+} catch (Exception $e) {
+    echo "<h2>Erro:</h2><pre>{$e->getMessage()}</pre>";
+}
+
+
+
 ?>
--- index.php
+++ index.php
@@ -1,41 +1,109 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>
-	Analisador Léxico
-</title>
-<link href="estilos/estilo.css" rel="stylesheet" type="text/css" />
-<script src="javascript/funcoes.js" type="text/javascript"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Analisador Léxico</title>
+    <link href="estilos/estilo.css" rel="stylesheet" type="text/css" />
+    <script src="javascript/funcoes.js" type="text/javascript"></script>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: #f4f4f4;
+        }
+
+        #corpo {
+            width: 800px;
+			height : 600px;
+            margin: auto;
+            background: white;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        #titulo {
+            text-align: center;
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        #entrada,
+        #saida {
+            margin-bottom: 20px;
+        }
+
+        textarea {
+            width: 100%;
+            height: 150px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            resize: none; /* Desabilita redimensionamento */
+        }
+
+        #status {
+            background: #e9ecef;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            max-height: 200px;
+            overflow: auto; /* Permite rolagem quando necessário */
+            white-space: pre-wrap; /* Mantém quebras de linha */
+        }
+
+        #controles {
+            text-align: center;
+        }
+
+        #botao {
+            padding: 10px 20px;
+            font-size: 16px;
+            color: white;
+            background-color: #007bff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        #botao:hover {
+            background-color: #0056b3; /* Efeito hover */
+        }
+    </style>
 </head>
 
-<body topmargin="0" leftmargin="0">
+<body>
 
 <div id="wait" style="display: none;">
-	<b>Analisando seu Código...</b>
+    <b>Analisando seu Código...</b>
 </div>
 
 <div id="corpo">
-	<div id="subcorpo">
-	
-		<div id="titulo">
-			Analisador Léxico
-		</div>
-		
-		<div id="entrada">
-			Código: <input type="text" name="codigo" id="codigo" maxlength="100">
-		</div>
-		
-		<div id="saida">
-			Resultado: 
-			<div id="status"></div>
-		</div>
-		
-	</div>
-	
-	<div id="controles">
-		<input id="botao" type="button" name="analisar" value="Analisar Lexicamente" onClick="analisar(GetObject('codigo').value);">
-	</div>
+    <div id="subcorpo">
+    
+        <div id="titulo">
+            Analisador Léxico
+        </div>
+        
+        <div id="entrada">
+            Código: 
+            <textarea name="codigo" id="codigo"></textarea>
+        </div>
+
+        <div id="saida">
+            Resultado: 
+            <div id="status"></div>
+        </div>
+        
+    </div>
+    
+    <div id="controles">
+		<br><br>
+        <input id="botao" type="button" name="analisar" value="Analisar Lexicamente" onClick="analisar(GetObject('codigo').value);">
+    </div>
 </div>
+
 </body>
 </html>
